05bc2588c3336468699067c63c44ae5c5442b6a6
srijith <sumakanthil@innovative-logistics.com>
2020-04-02 00:43:21 -0500

    enum added

--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -30,6 +30,12 @@ private:
 // can be either „red“ or „green“. Also, add the private method „void cycleThroughPhases()“. 
 // Furthermore, there shall be the private member _currentPhase which can take „red“ or „green“ as its value. 
 
+enum TrafficLightPhase
+{
+    red,
+    green 
+};
+
 class TrafficLight : public TrafficObject
 {
 public:
